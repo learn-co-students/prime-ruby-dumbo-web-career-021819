3b89848a1875eb822e4c5e2bd731c7d82c2dbeea
Jo K <joel.kohn42@gmail.com>
2019-01-09 22:55:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  return false if num <= 1
+  arr = (2..num).to_a 
+  
+  arr.each do |i| 
+    return false if num % i == 0 && num != i
+  end 
+  
+  true
+end 
