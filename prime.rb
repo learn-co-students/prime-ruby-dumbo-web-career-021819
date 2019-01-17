5fe32a33293ade2836fe3442d381066d7650d16e
William Robert Simpson <will.simpson85@gmail.com>
2019-01-17 15:45:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  (2..Math.sqrt(num)).each { |i|
+  if num % i == 0 && i < num
+    return false
+  end
+}
+   true
+ end
