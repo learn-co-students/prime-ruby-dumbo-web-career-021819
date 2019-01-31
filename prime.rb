68a01d914e03ceb1b69d997c5ef3610ec9487a3e
1-8192 <allegranzia@gmail.com>
2019-01-31 08:26:36 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  if num <= 0
+    return false
+  end
+  array = (1..num).to_a
+  factor_array = []
+  array.each do |factor|
+    if num % factor == 0
+      factor_array.push(factor)
+    end
+  end
+    if factor_array.length == 2
+      return true
+    else
+      return false
+ end
+end
