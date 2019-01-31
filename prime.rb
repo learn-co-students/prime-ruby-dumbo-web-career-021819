c08e5375d5658e86715f1526b0dd2ba067bde8bb
github username <github email address>
2019-01-31 17:37:39 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,16 +1,11 @@
-#def prime?(n)
-#  if n < 2
-#    return false
-#  end
-#    (2..n-1).each do |x|
-#      if (n % x) == 0
-#        return false
-#      end
-#    end
-#    true
-#end
-
 def prime?(n)
-  (2..n-1).each {|x| return false if (n % x) == 0}
-  true
+  if n < 2
+    return false
+  end
+    (2..n-1).each do |x|
+      if (n % x) == 0
+        return false
+      end
+    end
+    true
 end
