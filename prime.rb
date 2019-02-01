fbeccd30aa5c1187b494c66735edeb99ef4041e7
peidansauyukjuk <smei8800@gmail.com>
2019-02-01 14:50:04 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  return false if number == 1 || number == -1
+
+  last_number = (number.abs ** (0.5)).floor
+  x = 1..last_number
+  x = x.to_a
+  factor_list = []
+
+  x.each do |y|
+    factor_list.push(y) if number % y == 0
+  end
+
+  factor_list.size == 1 ? true:false
+end
