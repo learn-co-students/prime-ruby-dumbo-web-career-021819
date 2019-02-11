ea19448cdbcdffb486fed751c00ccf3b2e229f99
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-11 19:40:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+
+  square_root = Math.sqrt(number)
+  for x in 2..square_root do
+    if number % x == 0
+      return false
+  end
+end
+return true
+end
