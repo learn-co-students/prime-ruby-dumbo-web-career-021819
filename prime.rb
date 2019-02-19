fba6a6d558f2ed49a49ddee9c08d770a63a48a33
Jaela Wesley <jcwesley93@gmail.com>
2019-02-19 10:56:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n == 2
+    return true
+  elsif
+    integers = 2...n
+    integers.each do |int|
+      if n % int == 0
+        return false
+      end
+    end
+    return true
+ end
+end
